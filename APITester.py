cce3a16210247d79a43e8abc914deb5bd9392a77
ethansauerberg <ethansauerberg@wustl.edu>
2022-01-23 23:56:37 -0600

    Lots of work on automated testing

--- APITester.py
+++ APITester.py
@@ -1,23 +1,16 @@
+from pdb import post_mortem
 from random import random
 import requests
 import json 
 import random
 
-configsFile = open('configsTest.json')
-configs = json.load(configsFile)
-configs = configs["replacers"]
-configsFile.close()
-
-url = "http://" + configs["url"]
-
-emailChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-passChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%^&*()|:>?,/'[]~`"
-uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-lowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
-numbers = "1234567890"
-symbols = "!#$%^&*()|:>?,/'[]~`"
-
 def randomEmail():
+    emailChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    # passChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%^&*()|:>?,/'[]~`"
+    # uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    # lowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
+    # numbers = "1234567890"
+    # symbols = "!#$%^&*()|:>?,/'[]~`"
     length = random.randint(8, 15)
     email = ""
     while len(email) < length:
@@ -34,6 +27,12 @@ def randomEmail():
     return email
 
 def randomPassword():
+    # emailChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    passChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%^&*()|:>?,/'[]~`"
+    uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    lowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
+    numbers = "1234567890"
+    symbols = "!#$%^&*()|:>?,/'[]~`"
     length = random.randint(4, 96)
     password = ""
     while len(password) < length:
@@ -44,7 +43,88 @@ def randomPassword():
     password += symbols[random.randint(0, len(symbols) - 1)]
     print("random password generated: " + password)
     return password
-    
+
+def sendRequest(type, fullUrl, data):
+    try:
+        if type == "post":
+            postResponse = requests.post(fullUrl, data = data, headers = {})
+            postResponse.raise_for_status()
+            responseJson = postResponse.json()
+            return responseJson
+            # events = responseJson.get('events')
+        elif type == "get":
+            getResponse = requests.get(fullUrl, params=data, headers = {})
+            getResponse.raise_for_status()
+            responseJson = getResponse.json()
+            return responseJson
+            # events = responseJson.get('events')
+        elif type == "delete":
+            deleteResponse = requests.delete(fullUrl,data=data, headers={})
+            deleteResponse.raise_for_status()
+            deleteJson = deleteResponse.json()
+            return deleteJson
+    except requests.exceptions.HTTPError as errh:
+        print(errh)
+    except requests.exceptions.ConnectionError as errc:
+        print(errc)
+    except requests.exceptions.Timeout as errt:
+        print(errt)
+    except requests.exceptions.RequestException as err:
+        print(err)
+
+def recursiveSearch(query, input):
+    if isinstance(input, list) or isinstance(input, tuple):
+        for item in input:
+            if(recursiveSearch(query, item)):
+                return True
+        return False
+    elif isinstance(input, dict):
+        for key in input:
+            if(recursiveSearch(query, input[key])):
+                return True
+        return False
+    else:
+        return query in str(input)
+
+def runTests(tests):
+    print("Test results:\n")
+    for test in tests: 
+        requestReturn = sendRequest(test["testType"], test["testUrl"], test["testData"])
+        if recursiveSearch(test["expectedString"], requestReturn):
+            print(test["testName"] + "success")
+        else:
+            print(test["testName"] + "FAILURE")
+            print("\t" + str(requestReturn) + "\n")
+
+
+
+post = "post"
+get = "get"
+delete = "delete"
+
+# testEmail = randomEmail()
+# testPassword = randomPassword()
+# testBadEmail = randomEmail()
+# testBadPassword = randomPassword()
+testEmail = "asdf@asdf.asdf"
+testPassword = "123ASFasd!@#"
+
+configsFile = open('configsTest.json')
+configs = json.load(configsFile)
+configs = configs["replacers"]
+configsFile.close()
+
+url = "http://" + configs["url"]
+
+usersTests = [
+    # {"testName": "POST /users good: ", "expectedString": testEmail, "testType": post, "testUrl": url + "/" + configs["version"] + "/users", "testData" : {"email": testEmail, "password": testPassword,}},
+    # {"testName": "POST /users emailTaken: ", "expectedString": "Email Taken", "testType": post, "testUrl": url + "/" + configs["version"] + "/users", "testData" : {"email": testEmail, "password": testPassword,}},
+    # {"testName": "GET /users good: ", "expectedString": testEmail, "testType": get, "testUrl": url + "/" + configs["version"] + "/users/" + testEmail, "testData": {"password": testPassword}},
+    {"testName": "DELETE /users good: ", "expectedString": testEmail, "testType": delete, "testUrl": url + "/" + configs["version"] + "/users/" + testEmail, "testData": {"password": testPassword}},
+]
+
+runTests(usersTests)
+ 
 # users.js:
 # post user: one bad email (invalidEmail), one bad password (invalidPassword), one email that already exists (emailTaken), one good (returns user)
 # get user: one no results found (requestedResourceDidNotExist), one bad password (invalidEmailOrPassword), one bad ID (invalidMongoId), one good (returns user)
@@ -57,72 +137,6 @@ def randomPassword():
 # update one object: one bad username (invalidEmailOrPassword), one bad password (invalidEmailOrPassword), one bad ID (invalidMongoID), one wrong ID (requestedResourcesDidNotExist), one good (returns object)
 # delete one object: one bad username (invalidEmailOrPassword), one bad password (invalidEmailOrPassword), one bad ID (invalidMongoID), one wrong ID (requestedResourcesDidNotExist), one good (returns object)
 
-testEmail = randomEmail()
-testPassword = randomPassword()
-testBadEmail = randomEmail()
-testBadPassword = randomPassword()
-
-testEmail = "asdf@asdf.asdf"
-testPassword = "123ASFasd!@#"
-
-try:
-    getResponse = requests.post(url + "/" + configs["version"] + "/users",
-    data = {
-        "email": testEmail,
-        "password": testPassword,
-    },
-        headers = {}
-        )
-    getResponse.raise_for_status()
-    responseJson = getResponse.json()
-    print(responseJson)
-    events = responseJson.get('events')
-except requests.exceptions.HTTPError as errh:
-    print(errh)
-except requests.exceptions.ConnectionError as errc:
-    print(errc)
-except requests.exceptions.Timeout as errt:
-    print(errt)
-except requests.exceptions.RequestException as err:
-    print(err)
-
-
-try:
-    getResponse = requests.get(url + "/" + configs["version"] + "/users/:" + testEmail,
-    params={
-        "password": testPassword,
-    },
-        headers = {}
-        )
-    getResponse.raise_for_status()
-    responseJson = getResponse.json()
-    print(responseJson)
-    events = responseJson.get('events')
-except requests.exceptions.HTTPError as errh:
-    print(errh)
-except requests.exceptions.ConnectionError as errc:
-    print(errc)
-except requests.exceptions.Timeout as errt:
-    print(errt)
-except requests.exceptions.RequestException as err:
-    print(err)
-
-# try:
-#     postResponse = requests.post("",
-#         data = {},
-#         headers = {'Content-type': 'application/json;charset=utf-8', 'Accept': '*/*'}
-#         )
-#     print(postResponse.json())
-#     postResponse.raise_for_status()
-#     if postResponse.status_code == 200:
-#         print("200 returned; data posted")
-#     else: 
-#         print(postResponse)
-# except requests.exceptions.HTTPError as errh:
-#     print(errh)
-# except requests.exceptions.ConnectionError as errc:
-#     print(errc)
-# except requests.exceptions.Timeout as errt:
-#     print(errt)
-# except requests.exceptions.RequestException as err:
-#     print(err)
+    
+# array of objects containing reqType, url, data, and expected string in return.data
+# for each object, send it's request, then confirm that it's expected string is in the return.
--- README.md
+++ README.md
@@ -1,1 +1,133 @@
-# Automated-APIs
+<div id="top"></div>
+
+[![Contributors][contributors-shield]][contributors-url]
+[![Forks][forks-shield]][forks-url]
+[![Stargazers][stars-shield]][stars-url]
+[![Issues][issues-shield]][issues-url]
+[![MIT License][license-shield]][license-url]
+[![LinkedIn][linkedin-shield]][linkedin-url]
+
+
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+  <!-- <a href="https://github.com/github_username/repo_name">
+    <img src="images/logo.png" alt="Logo" width="80" height="80">
+  </a> -->
+
+<h3 align="center">Auto API</h3>
+
+  <p align="center">
+    unlimited, free (Node.js/Express.js/MongoDB) API's with custom user-defined types
+    <br>
+    <br>
+    This project is still in progress, but (my school workload permitting) I'm hoping to finish soon!
+    <br>
+    I will eventually put this up on my person website with a full guide including usage, hosting for free on AWS, setting up a free MongoDB database, etc :)
+
+    <!-- <br />
+    <a href="https://github.com/github_username/repo_name"><strong>Explore the docs »</strong></a>
+    <br />
+    <br />
+    <a href="https://github.com/github_username/repo_name">View Demo</a>
+    ·
+    <a href="https://github.com/github_username/repo_name/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/github_username/repo_name/issues">Request Feature</a> -->
+  </p>
+</div>
+
+
+
+<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#prerequisites">Prerequisites</a></li>
+        <li><a href="#installation">Installation</a></li>
+      </ul>
+    </li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#roadmap">Roadmap</a></li>
+    <li><a href="#contributing">Contributing</a></li>
+    <li><a href="#license">License</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#acknowledgments">Acknowledgments</a></li>
+  </ol>
+</details>
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+[![Product Name Screen Shot][product-screenshot]](https://example.com)
+
+Here's a blank template to get started: To avoid retyping too much info. Do a search and replace with your text editor for the following: `github_username`, `repo_name`, `twitter_handle`, `linkedin_username`, `email`, `email_client`, `project_title`, `project_description`
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+### Built With
+
+* [MongoDB]
+* [NodeJS]
+* [ExpressJS]
+<!-- * [Next.js](https://nextjs.org/) -->
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+This section will eventually be the guide
+
+
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+This section will eventually contain an example or two of uses
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- LICENSE -->
+## License
+
+To be honest, I know almost nothing about licenses. I have little interest in selling this code and hope to distribute it for free with a simple promise of use for good :)
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+[contributors-shield]: https://img.shields.io/github/contributors/github_username/repo_name.svg?style=for-the-badge
+[contributors-url]: https://github.com/github_username/repo_name/graphs/contributors
+[forks-shield]: https://img.shields.io/github/forks/github_username/repo_name.svg?style=for-the-badge
+[forks-url]: https://github.com/github_username/repo_name/network/members
+[stars-shield]: https://img.shields.io/github/stars/github_username/repo_name.svg?style=for-the-badge
+[stars-url]: https://github.com/github_username/repo_name/stargazers
+[issues-shield]: https://img.shields.io/github/issues/github_username/repo_name.svg?style=for-the-badge
+[issues-url]: https://github.com/github_username/repo_name/issues
+[license-shield]: https://img.shields.io/github/license/github_username/repo_name.svg?style=for-the-badge
+[license-url]: https://github.com/github_username/repo_name/blob/master/LICENSE.txt
+[linkedin-shield]: https://img.shields.io/badge/-LinkedIn-black.svg?style=for-the-badge&logo=linkedin&colorB=555
+[linkedin-url]: https://linkedin.com/in/linkedin_username
+[product-screenshot]: images/screenshot.png
